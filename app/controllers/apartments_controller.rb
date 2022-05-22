4c1298bee0a748a858e0e9b10860f3be8611b0aa
Dustin <42046575+dustin-oz@users.noreply.github.com>
2022-05-22 07:26:31 -0500

    Added Update route/method to controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -13,15 +13,15 @@ class ApartmentsController < ApplicationController
         end
       end
 
-    #   def update
-    #     apartment = Apartment.find(params[:id])
-    #     apartment.update(apartment_params)
-    #     if apartment.valid?
-    #       render json: apartment
-    #     else
-    #       render json: apartment.errors, status: :unprocessable_entity
-    #     end
-    #   end
+      def update
+        apartment = Apartment.find(params[:id])
+        apartment.update(apartment_params)
+        if apartment.valid?
+          render json: apartment
+        else
+          render json: apartment.errors, status: :unprocessable_entity
+        end
+      end
 
     #   def destroy
     #     apartment = Apartment.find(params[:id])
